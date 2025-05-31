c7cf102bd624f6c6a5f8359a98e4ea4ac121ca36
Egor Tsyganchuk <cyaneor@gmail.com>
2025-06-01 02:02:21 +0300

    Changed error structure verification test

--- test/src/error.cpp
+++ test/src/error.cpp
@@ -3,46 +3,54 @@
 
 constexpr const auto EYA_ERROR_CODE_SOME_ERROR = 666;
 
-TEST(eya_error, get_code) {
-  eya_error_t err = {15};
+TEST(eya_error, get_code)
+{
+  eya_error_t err{15};
   EXPECT_EQ(eya_error_get_code(&err), 15);
 }
 
-TEST(eya_error, get_code_null) {
+TEST(eya_error, GetCodeNull)
+{
   EXPECT_DEATH(eya_error_get_code(nullptr), ".*");
 }
 
-TEST(eya_error, get_desc) {
-  eya_error_t err = {.desc = "Test error"};
+TEST(eya_error, get_desc)
+{
+  eya_error_t err{0, "Test error"};
   EXPECT_STREQ(eya_error_get_desc(&err), "Test error");
 }
 
-TEST(eya_error, get_desc_null) {
+TEST(eya_error, get_desc_null)
+{
   EXPECT_DEATH(eya_error_get_desc(nullptr), ".*");
 }
 
-TEST(eya_error, set) {
-  eya_error_t err = {};
+TEST(eya_error, set)
+{
+  eya_error_t err{};
   eya_error_set(&err, EYA_ERROR_CODE_SOME_ERROR, "Test error");
 
   EXPECT_EQ(err.code, EYA_ERROR_CODE_SOME_ERROR);
   EXPECT_STREQ(err.desc, "Test error");
 }
 
-TEST(eya_error, set_null) {
+TEST(eya_error, SetNull)
+{
   EXPECT_DEATH(eya_error_set(nullptr, EYA_ERROR_CODE_SOME_ERROR, "Test error"),
                ".*");
 }
 
-TEST(eya_error, set_code) {
-  eya_error_t err = {};
+TEST(eya_error, set_code)
+{
+  eya_error_t err{};
   eya_error_set_code(&err, EYA_ERROR_CODE_SOME_ERROR);
   EXPECT_EQ(err.code, EYA_ERROR_CODE_SOME_ERROR);
   EXPECT_EQ(err.desc, nullptr);
 }
 
-TEST(eya_error, unpack) {
-  eya_error_t err = {.code = EYA_ERROR_CODE_SOME_ERROR, .desc = "Test error"};
+TEST(eya_error, unpack)
+{
+  eya_error_t err{EYA_ERROR_CODE_SOME_ERROR, "Test error"};
   eya_error_code_t code;
   const char *desc;
   eya_error_unpack(&err, &code, &desc);
@@ -50,8 +58,9 @@ TEST(eya_error, unpack) {
   EXPECT_STREQ(desc, "Test error");
 }
 
-TEST(eya_error, unpack_partial) {
-  eya_error_t err = {.code = EYA_ERROR_CODE_SOME_ERROR, .desc = "Test error"};
+TEST(eya_error, unpack_partial)
+{
+  eya_error_t err{EYA_ERROR_CODE_SOME_ERROR, "Test error"};
 
   eya_error_code_t code;
   eya_error_unpack(&err, &code, nullptr);
@@ -62,46 +71,54 @@ TEST(eya_error, unpack_partial) {
   EXPECT_STREQ(desc, "Test error");
 }
 
-TEST(eya_error, assign) {
-  eya_error_t err = {};
-  eya_error_t other = {.code = EYA_ERROR_CODE_SOME_ERROR, .desc = "Test error"};
+TEST(eya_error, assign)
+{
+  eya_error_t err{};
+  eya_error_t other{EYA_ERROR_CODE_SOME_ERROR, "Test error"};
   eya_error_assign(&err, &other);
   EXPECT_EQ(err.code, EYA_ERROR_CODE_SOME_ERROR);
   EXPECT_STREQ(err.desc, "Test error");
 }
 
-TEST(eya_error, clear) {
-  eya_error_t err = {.code = EYA_ERROR_CODE_SOME_ERROR, .desc = "Test error"};
+TEST(eya_error, clear)
+{
+  eya_error_t err{EYA_ERROR_CODE_SOME_ERROR, "Test error"};
   eya_error_clear(&err);
   EXPECT_EQ(err.code, EYA_ERROR_CODE_NONE);
   EXPECT_EQ(err.desc, nullptr);
 }
 
-TEST(eya_error, get_code_and_clear) {
-  eya_error_t err = {.code = EYA_ERROR_CODE_SOME_ERROR, .desc = "Test error"};
+TEST(eya_error, get_code_and_clear)
+{
+  eya_error_t err{EYA_ERROR_CODE_SOME_ERROR, "Test error"};
   eya_error_code_t code = eya_error_get_code_and_clear(&err);
   EXPECT_EQ(code, EYA_ERROR_CODE_SOME_ERROR);
   EXPECT_EQ(err.code, EYA_ERROR_CODE_NONE);
   EXPECT_EQ(err.desc, nullptr);
 }
 
-TEST(eya_error, is_code) {
-  eya_error_t err = {.code = EYA_ERROR_CODE_SOME_ERROR, .desc = "Test error"};
+TEST(eya_error, is_code)
+{
+  eya_error_t err{EYA_ERROR_CODE_SOME_ERROR, "Test error"};
   EXPECT_TRUE(eya_error_is_code(&err, EYA_ERROR_CODE_SOME_ERROR));
   EXPECT_FALSE(eya_error_is_code(&err, EYA_ERROR_CODE_NONE));
 }
 
-TEST(eya_error, is_code_equal) {
-  eya_error_t err1 = {.code = EYA_ERROR_CODE_SOME_ERROR, .desc = "Test error"};
-  eya_error_t err2 = {.code = EYA_ERROR_CODE_SOME_ERROR, .desc = "Other error"};
+TEST(eya_error, is_code_equal)
+{
+  eya_error_t err1{EYA_ERROR_CODE_SOME_ERROR, "Test error"};
+  eya_error_t err2{EYA_ERROR_CODE_SOME_ERROR, "Other error"};
   EXPECT_TRUE(eya_error_is_code_equal(&err1, &err2));
-  eya_error_t err3 = {.code = EYA_ERROR_CODE_NONE, .desc = "Test error"};
+
+  eya_error_t err3{EYA_ERROR_CODE_NONE, "Test error"};
   EXPECT_FALSE(eya_error_is_code_equal(&err1, &err3));
 }
 
-TEST(eya_error, is_ok) {
-  eya_error_t err = {.code = EYA_ERROR_CODE_NONE, .desc = nullptr};
+TEST(eya_error, is_ok)
+{
+  eya_error_t err{EYA_ERROR_CODE_NONE, nullptr};
   EXPECT_TRUE(eya_error_is_ok(&err));
-  eya_error_t err2 = {.code = EYA_ERROR_CODE_SOME_ERROR, .desc = "Test error"};
+
+  eya_error_t err2{EYA_ERROR_CODE_SOME_ERROR, "Test error"};
   EXPECT_FALSE(eya_error_is_ok(&err2));
 }
