492706d8cfd81e7bddb04195161883907b8eb8f9
Egor Tsyganchuk <cyaneor@gmail.com>
2025-06-04 07:24:14 +0300

    Added version.c file for assembly

--- src/cmake/module/source_files.cmake
+++ src/cmake/module/source_files.cmake
@@ -4,6 +4,7 @@ list(APPEND EYA_TARGET_SOURCE_FILES
         ${EYA_LIB_SOURCE_DIR}/eya/runtime_terminate.c
         ${EYA_LIB_SOURCE_DIR}/eya/error.c
         ${EYA_LIB_SOURCE_DIR}/eya/memory.c
+        ${EYA_LIB_SOURCE_DIR}/eya/version.c
         ${EYA_LIB_SOURCE_DIR}/eya/memory_std.c
         ${EYA_LIB_SOURCE_DIR}/eya/memory_raw.c
         ${EYA_LIB_SOURCE_DIR}/eya/memory_allocator.c
