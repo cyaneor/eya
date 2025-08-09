be57e09e96f7d7c514e6121f161e6f3cb4d1e427
Egor Tsyganchuk <cyaneor@gmail.com>
2025-08-10 00:05:20 +0300

    process_cmake_options added optional parameter for handling options with value

--- lib/cmake/modules/process_cmake_options.cmake
+++ lib/cmake/modules/process_cmake_options.cmake
@@ -5,14 +5,15 @@
 # Options matching the criteria can be collected in an output list.                        #
 #                                                                                          #
 # Parameters:                                                                              #
-#   OPTION_PREFIX  - Prefix that options should start with (single value)                  #
-#   OPTION_VALUE   - Required value for matching options (single value)                    #
-#   OUTPUT_LIST    - Name of the output variable to store matching options (optional)      #
-#   OPTION_LIST    - List of options to process (multi-value)                              #
+#   OPTION_PREFIX      - Prefix that options should start with (single value)              #
+#   OPTION_VALUE       - Required value for matching options (single value)                #
+#   OUTPUT_LIST        - Name of the output variable to store matching options (optional)  #
+#   OUTPUT_WITH_VALUES - If set, output will be in NAME=VALUE format (optional)            #
+#   OPTION_LIST        - List of options to process (multi-value)                          #
 # ---------------------------------------------------------------------------------------- #
 function(process_cmake_options)
     # Parse function arguments
-    set(oneValueArgs OPTION_PREFIX OPTION_VALUE OUTPUT_LIST)
+    set(oneValueArgs OPTION_PREFIX OPTION_VALUE OUTPUT_LIST OUTPUT_WITH_VALUES)
     set(multiValueArgs OPTION_LIST)
     cmake_parse_arguments(ARGS "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
@@ -20,7 +21,7 @@ function(process_cmake_options)
     if (NOT ARGS_OPTION_LIST OR NOT ARGS_OPTION_PREFIX OR NOT DEFINED ARGS_OPTION_VALUE)
         message(FATAL_ERROR "Missing required parameters: OPTION_LIST, OPTION_PREFIX or OPTION_VALUE")
     endif ()
-    
+
     # Process all options
     foreach (CURRENT_OPTION IN ITEMS ${ARGS_OPTION_LIST})
         # Check prefix and value match
@@ -30,7 +31,19 @@ function(process_cmake_options)
 
             # Add to output list if specified
             if (ARGS_OUTPUT_LIST)
-                list(APPEND ${ARGS_OUTPUT_LIST} ${CURRENT_OPTION})
+                if (ARGS_OUTPUT_WITH_VALUES)
+                    # Convert ON/OFF to 1/0 if needed
+                    if (${CURRENT_OPTION} STREQUAL "ON")
+                        set(VALUE_TO_OUTPUT "1")
+                    elseif (${CURRENT_OPTION} STREQUAL "OFF")
+                        set(VALUE_TO_OUTPUT "0")
+                    else ()
+                        set(VALUE_TO_OUTPUT "${${CURRENT_OPTION}}")
+                    endif ()
+                    list(APPEND ${ARGS_OUTPUT_LIST} "${CURRENT_OPTION}=${VALUE_TO_OUTPUT}")
+                else ()
+                    list(APPEND ${ARGS_OUTPUT_LIST} ${CURRENT_OPTION})
+                endif ()
             endif ()
         endif ()
     endforeach ()
