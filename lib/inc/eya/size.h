3b488d11a4e68b1081f949622860d65d614df149
Egor Tsyganchuk <cyaneor@gmail.com>
2025-08-12 20:04:32 +0300

    Definition of size_t for the transmitted size from CMake was removed

--- lib/inc/eya/size.h
+++ lib/inc/eya/size.h
@@ -1,13 +1,12 @@
 /**
  * @file size.h
- * @brief Header file defining signed
- *        and unsigned size types (`eya_ssize_t` and `eya_usize_t`).
+ * @brief Platform-specific size type definitions
  *
- * This header defines platform-appropriate size types (signed and unsigned)
- * with their corresponding minimum and maximum values.
+ * This header defines platform-specific signed and unsigned size types
+ * (eya_ssize_t and eya_usize_t) along with their minimum and maximum values.
  *
- * The implementation varies depending on the platform (Windows vs others)
- * and can be customized by defining EYA_SIZE_T_SIZE.
+ * The actual types depend on the platform (Windows vs others)
+ * and bit depth (32-bit vs 64-bit on Windows).
  */
 
 #ifndef EYA_SIZE_H
@@ -15,307 +14,155 @@
 
 #include "numeric_fixed.h"
 
-#ifndef EYA_SIZE_T_SIZE
-#    if EYA_COMPILER_OS_TYPE == EYA_COMPILER_OS_TYPE_WINDOWS
-
-#        if EYA_COMPILER_BIT_DEPTH == 64
-#            define EYA_SIZE_T_SIZE EYA_LLONG_T_SIZE
+#if EYA_COMPILER_OS_TYPE == EYA_COMPILER_OS_TYPE_WINDOWS
+#    if EYA_COMPILER_BIT_DEPTH == 64
+/**
+ * @def EYA_SIZE_T_SIZE
+ * @brief Size type identifier for 64-bit Windows platforms
+ * @details Defined as EYA_LLONG_T_SIZE indicating long long type size is used
+ */
+#        define EYA_SIZE_T_SIZE EYA_LLONG_T_SIZE
 
 /**
  * @def EYA_SSIZE_T_MIN
  * @brief Minimum value for signed size type (Windows, 64-bit)
+ * @details Defined as EYA_SLLONG_T_MIN for 64-bit Windows platforms
  */
-#            define EYA_SSIZE_T_MIN EYA_SLLONG_T_MIN
+#        define EYA_SSIZE_T_MIN EYA_SLLONG_T_MIN
 
 /**
  * @def EYA_SSIZE_T_MAX
  * @brief Maximum value for signed size type (Windows, 64-bit)
+ * @details Defined as EYA_SLLONG_T_MAX for 64-bit Windows platforms
  */
-#            define EYA_SSIZE_T_MAX EYA_SLLONG_T_MAX
+#        define EYA_SSIZE_T_MAX EYA_SLLONG_T_MAX
 
 /**
  * @typedef eya_ssize_t
  * @brief Signed integer type for representing sizes or counts (Windows, 64-bit)
+ * @details Alias for eya_s64_t on 64-bit Windows platforms
  */
 typedef eya_s64_t eya_ssize_t;
 
 /**
  * @def EYA_USIZE_T_MIN
  * @brief Minimum value (0) for unsigned size type (Windows, 64-bit)
+ * @details Defined as EYA_ULLONG_T_MIN (0) for 64-bit Windows platforms
  */
-#            define EYA_USIZE_T_MIN EYA_ULLONG_T_MIN
+#        define EYA_USIZE_T_MIN EYA_ULLONG_T_MIN
 
 /**
  * @def EYA_USIZE_T_MAX
  * @brief Maximum value for unsigned size type (Windows, 64-bit)
+ * @details Defined as EYA_ULLONG_T_MAX for 64-bit Windows platforms
  */
-#            define EYA_USIZE_T_MAX EYA_ULLONG_T_MAX
+#        define EYA_USIZE_T_MAX EYA_ULLONG_T_MAX
 
 /**
  * @typedef eya_usize_t
  * @brief Unsigned integer type for representing sizes or counts (Windows, 64-bit)
+ * @details Alias for eya_u64_t on 64-bit Windows platforms
  */
 typedef eya_u64_t eya_usize_t;
-#        else
-#            define EYA_SIZE_T_SIZE EYA_INT_T_SIZE
+#    else /* EYA_COMPILER_OS_TYPE != EYA_COMPILER_OS_TYPE_WINDOWS */
+/**
+ * @def EYA_SIZE_T_SIZE
+ * @brief Size type identifier for 32-bit Windows platforms
+ * @details Defined as EYA_INT_T_SIZE indicating int type size is used
+ */
+#        define EYA_SIZE_T_SIZE EYA_INT_T_SIZE
 
 /**
  * @def EYA_SSIZE_T_MIN
  * @brief Minimum value for signed size type (Windows, 32-bit)
+ * @details Defined as EYA_SINT_T_MIN for 32-bit Windows platforms
  */
-#            define EYA_SSIZE_T_MIN EYA_SINT_T_MIN
+#        define EYA_SSIZE_T_MIN EYA_SINT_T_MIN
 
 /**
  * @def EYA_SSIZE_T_MAX
  * @brief Maximum value for signed size type (Windows, 32-bit)
+ * @details Defined as EYA_SINT_T_MAX for 32-bit Windows platforms
  */
-#            define EYA_SSIZE_T_MAX EYA_SINT_T_MAX
+#        define EYA_SSIZE_T_MAX EYA_SINT_T_MAX
 
 /**
  * @typedef eya_ssize_t
  * @brief Signed integer type for representing sizes or counts (Windows, 32-bit)
+ * @details Alias for eya_sint_t on 32-bit Windows platforms
  */
 typedef eya_sint_t eya_ssize_t;
 
 /**
  * @def EYA_USIZE_T_MIN
  * @brief Minimum value (0) for unsigned size type (Windows, 32-bit)
+ * @details Defined as EYA_UINT_T_MIN (0) for 32-bit Windows platforms
  */
-#            define EYA_USIZE_T_MIN EYA_UINT_T_MIN
+#        define EYA_USIZE_T_MIN EYA_UINT_T_MIN
 
 /**
  * @def EYA_USIZE_T_MAX
  * @brief Maximum value for unsigned size type (Windows, 32-bit)
+ * @details Defined as EYA_UINT_T_MAX for 32-bit Windows platforms
  */
-#            define EYA_USIZE_T_MAX EYA_UINT_T_MAX
+#        define EYA_USIZE_T_MAX EYA_UINT_T_MAX
 
 /**
  * @typedef eya_usize_t
  * @brief Unsigned integer type for representing sizes or counts (Windows, 32-bit)
+ * @details Alias for eya_uint_t on 32-bit Windows platforms
  */
 typedef eya_uint_t eya_usize_t;
-#        endif
-
-#    else
-#        define EYA_SIZE_T_SIZE EYA_LONG_T_SIZE
+#    endif
+#else /* EYA_COMPILER_OS_TYPE == EYA_COMPILER_OS_TYPE_WINDOWS */
+/**
+ * @def EYA_SIZE_T_SIZE
+ * @brief Size type identifier for non-Windows platforms
+ * @details Defined as EYA_LONG_T_SIZE indicating long type size is used
+ */
+#    define EYA_SIZE_T_SIZE EYA_LONG_T_SIZE
 
 /**
  * @def EYA_SSIZE_T_MIN
  * @brief Minimum value for platform's native signed size type
+ * @details Defined as EYA_SLONG_T_MIN for non-Windows platforms
  */
-#        define EYA_SSIZE_T_MIN EYA_SLONG_T_MIN
+#    define EYA_SSIZE_T_MIN EYA_SLONG_T_MIN
 
 /**
  * @def EYA_SSIZE_T_MAX
  * @brief Maximum value for platform's native signed size type
+ * @details Defined as EYA_SLONG_T_MAX for non-Windows platforms
  */
-#        define EYA_SSIZE_T_MAX EYA_SLONG_T_MAX
+#    define EYA_SSIZE_T_MAX EYA_SLONG_T_MAX
 
 /**
  * @typedef eya_ssize_t
  * @brief Platform's native signed type for representing sizes or counts
+ * @details Alias for eya_slong_t on non-Windows platforms
  */
 typedef eya_slong_t eya_ssize_t;
 
 /**
  * @def EYA_USIZE_T_MIN
  * @brief Minimum value (0) for platform's native unsigned size type
+ * @details Defined as EYA_ULONG_T_MIN (0) for non-Windows platforms
  */
-#        define EYA_USIZE_T_MIN EYA_ULONG_T_MIN
+#    define EYA_USIZE_T_MIN EYA_ULONG_T_MIN
 
 /**
  * @def EYA_USIZE_T_MAX
  * @brief Maximum value for platform's native unsigned size type
+ * @details Defined as EYA_ULONG_T_MAX for non-Windows platforms
  */
-#        define EYA_USIZE_T_MAX EYA_ULONG_T_MAX
+#    define EYA_USIZE_T_MAX EYA_ULONG_T_MAX
 
 /**
  * @typedef eya_usize_t
  * @brief Platform's native unsigned type for representing sizes or counts
+ * @details Alias for eya_ulong_t on non-Windows platforms
  */
 typedef eya_ulong_t eya_usize_t;
-#    endif
-#else
-#    if EYA_SIZE_T_SIZE == 8
-#        ifndef EYA_SSIZE_T_MIN
-/**
- * @def EYA_SSIZE_T_MIN
- * @brief Minimum value for signed size type (64-bit)
- */
-#            define EYA_SSIZE_T_MIN EYA_S64_T_MIN
-#        endif
-
-#        ifndef EYA_SSIZE_T_MAX
-/**
- * @def EYA_SSIZE_T_MAX
- * @brief Maximum value for signed size type (64-bit)
- */
-#            define EYA_SSIZE_T_MAX EYA_S64_T_MAX
-#        endif
-
-/**
- * @typedef eya_ssize_t
- * @brief Signed 64-bit integer type for representing sizes or counts
- */
-typedef eya_s64_t eya_ssize_t;
-
-#        ifndef EYA_USIZE_T_MIN
-/**
- * @def EYA_USIZE_T_MIN
- * @brief Minimum value (0) for unsigned size type (64-bit)
- */
-#            define EYA_USIZE_T_MIN EYA_U64_T_MIN
-#        endif
-
-#        ifndef EYA_USIZE_T_MAX
-/**
- * @def EYA_USIZE_T_MAX
- * @brief Maximum value for unsigned size type (64-bit)
- */
-#            define EYA_USIZE_T_MAX EYA_U64_T_MAX
-#        endif
-
-/**
- * @typedef eya_usize_t
- * @brief Unsigned 64-bit integer type for representing sizes or counts
- */
-typedef eya_u64_t eya_usize_t;
-
-#    elif EYA_SIZE_T_SIZE == 4
-#        ifndef EYA_SSIZE_T_MIN
-/**
- * @def EYA_SSIZE_T_MIN
- * @brief Minimum value for signed size type (32-bit)
- */
-#            define EYA_SSIZE_T_MIN EYA_S32_T_MIN
-#        endif
-
-#        ifndef EYA_SSIZE_T_MAX
-/**
- * @def EYA_SSIZE_T_MAX
- * @brief Maximum value for signed size type (32-bit)
- */
-#            define EYA_SSIZE_T_MAX EYA_S32_T_MAX
-#        endif
-
-/**
- * @typedef eya_ssize_t
- * @brief Signed 32-bit integer type for representing sizes or counts
- */
-typedef eya_s32_t eya_ssize_t;
-
-#        ifndef EYA_USIZE_T_MIN
-/**
- * @def EYA_USIZE_T_MIN
- * @brief Minimum value (0) for unsigned size type (32-bit)
- */
-#            define EYA_USIZE_T_MIN EYA_U32_T_MIN
-#        endif
-
-#        ifndef EYA_USIZE_T_MAX
-/**
- * @def EYA_USIZE_T_MAX
- * @brief Maximum value for unsigned size type (32-bit)
- */
-#            define EYA_USIZE_T_MAX EYA_U32_T_MAX
-#        endif
-
-/**
- * @typedef eya_usize_t
- * @brief Unsigned 32-bit integer type for representing sizes or counts
- */
-typedef eya_u32_t eya_usize_t;
-
-#    elif EYA_SIZE_T_SIZE == 2
-#        ifndef EYA_SSIZE_T_MIN
-/**
- * @def EYA_SSIZE_T_MIN
- * @brief Minimum value for signed size type (16-bit)
- */
-#            define EYA_SSIZE_T_MIN EYA_S16_T_MIN
-#        endif
-
-#        ifndef EYA_SSIZE_T_MAX
-/**
- * @def EYA_SSIZE_T_MAX
- * @brief Maximum value for signed size type (16-bit)
- */
-#            define EYA_SSIZE_T_MAX EYA_S16_T_MAX
-#        endif
-
-/**
- * @typedef eya_ssize_t
- * @brief Signed 16-bit integer type for representing sizes or counts
- */
-typedef eya_s16_t eya_ssize_t;
-
-#        ifndef EYA_USIZE_T_MIN
-/**
- * @def EYA_USIZE_T_MIN
- * @brief Minimum value (0) for unsigned size type (16-bit)
- */
-#            define EYA_USIZE_T_MIN EYA_U16_T_MIN
-#        endif
-
-#        ifndef EYA_USIZE_T_MAX
-/**
- * @def EYA_USIZE_T_MAX
- * @brief Maximum value for unsigned size type (16-bit)
- */
-#            define EYA_USIZE_T_MAX EYA_U16_T_MAX
-#        endif
-
-/**
- * @typedef eya_usize_t
- * @brief Unsigned 16-bit integer type for representing sizes or counts
- */
-typedef eya_u16_t eya_usize_t;
-
-#    elif EYA_SIZE_T_SIZE == 1
-#        ifndef EYA_SSIZE_T_MIN
-/**
- * @def EYA_SSIZE_T_MIN
- * @brief Minimum value for signed size type (8-bit)
- */
-#            define EYA_SSIZE_T_MIN EYA_S8_T_MIN
-#        endif
-
-#        ifndef EYA_SSIZE_T_MAX
-/**
- * @def EYA_SSIZE_T_MAX
- * @brief Maximum value for signed size type (8-bit)
- */
-#            define EYA_SSIZE_T_MAX EYA_S8_T_MAX
-#        endif
-
-/**
- * @typedef eya_ssize_t
- * @brief Signed 8-bit integer type for representing sizes or counts
- */
-typedef eya_s8_t eya_ssize_t;
-
-#        ifndef EYA_USIZE_T_MIN
-/**
- * @def EYA_USIZE_T_MIN
- * @brief Minimum value (0) for unsigned size type (8-bit)
- */
-#            define EYA_USIZE_T_MIN EYA_U8_T_MIN
-#        endif
-
-#        ifndef EYA_USIZE_T_MAX
-/**
- * @def EYA_USIZE_T_MAX
- * @brief Maximum value for unsigned size type (8-bit)
- */
-#            define EYA_USIZE_T_MAX EYA_U8_T_MAX
-#        endif
-
-/**
- * @typedef eya_usize_t
- * @brief Unsigned 8-bit integer type for representing sizes or counts
- */
-typedef eya_u8_t eya_usize_t;
-#    endif
-#endif // EYA_SIZE_T_SIZE
+#endif
 
 #endif // EYA_SIZE_H
