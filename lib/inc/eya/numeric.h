297caf2e325487df0d5e824cc0ea6f471a392c43
Egor Tsyganchuk <cyaneor@gmail.com>
2025-08-27 07:20:21 +0300

    Fixed Doxygen documentation numeric.h

--- lib/inc/eya/numeric.h
+++ lib/inc/eya/numeric.h
@@ -6,10 +6,6 @@
  *          It provides a unified interface to access all numeric type
  *          definitions and their associated limits and properties.
  *
- * The header includes:
- * - @ref numeric_types.h: Type definitions for signed and unsigned integers
- * - @ref numeric_limits.h: Size and limit macros for numeric types
- *
  * @note This is a convenience header that aggregates the complete numeric
  *       types functionality. For fine-grained control, include the specific
  *       headers directly.
@@ -22,5 +18,3 @@
 #    include "numeric_limits.h" ///< Size and limit macros for numeric types
 
 #endif // EYA_NUMERIC_H
-
-/** @} */ // end of numeric group
