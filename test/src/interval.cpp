edde96bd8b07b37891111551036338017ceed1a5
Egor Tsyganchuk <cyaneor@gmail.com>
2025-09-07 20:03:51 +0300

    Added tests for checking float ranges

--- test/src/interval.cpp
+++ test/src/interval.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <eya/interval_util.h>
 
-TEST(eya_interval_contains_value, closed_integer_values)
+TEST(eya_interval_contains_value_closed, integer_values)
 {
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_CLOSED, 1, 3, 5));
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_CLOSED, 1, 1, 5));
@@ -10,7 +10,7 @@ TEST(eya_interval_contains_value, closed_integer_values)
     EXPECT_FALSE(eya_interval_contains_value(EYA_INTERVAL_TYPE_CLOSED, 1, 6, 5));
 }
 
-TEST(eya_interval_contains_value, closed_float_values)
+TEST(eya_interval_contains_value_closed_float, float_values)
 {
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_CLOSED, 1.0, 3.0, 5.0));
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_CLOSED, 1.0, 1.0, 5.0));
@@ -19,7 +19,7 @@ TEST(eya_interval_contains_value, closed_float_values)
     EXPECT_FALSE(eya_interval_contains_value(EYA_INTERVAL_TYPE_CLOSED, 1.0, 5.1, 5.0));
 }
 
-TEST(eya_interval_contains_value, left_open_integer_values)
+TEST(eya_interval_contains_value_left_open, integer_values)
 {
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_LEFT_OPEN, 1, 3, 5));
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_LEFT_OPEN, 1, 5, 5));
@@ -28,7 +28,7 @@ TEST(eya_interval_contains_value, left_open_integer_values)
     EXPECT_FALSE(eya_interval_contains_value(EYA_INTERVAL_TYPE_LEFT_OPEN, 1, 6, 5));
 }
 
-TEST(eya_interval_contains_value, right_open_integer_values)
+TEST(eya_interval_contains_value_right_open, integer_values)
 {
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_RIGHT_OPEN, 1, 3, 5));
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_RIGHT_OPEN, 1, 1, 5));
@@ -37,43 +37,11 @@ TEST(eya_interval_contains_value, right_open_integer_values)
     EXPECT_FALSE(eya_interval_contains_value(EYA_INTERVAL_TYPE_RIGHT_OPEN, 1, 6, 5));
 }
 
-TEST(eya_interval_contains_value, open_integer_values)
+TEST(eya_interval_contains_value_open, integer_values)
 {
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_OPEN, 1, 3, 5));
     EXPECT_FALSE(eya_interval_contains_value(EYA_INTERVAL_TYPE_OPEN, 1, 1, 5));
     EXPECT_FALSE(eya_interval_contains_value(EYA_INTERVAL_TYPE_OPEN, 1, 5, 5));
     EXPECT_FALSE(eya_interval_contains_value(EYA_INTERVAL_TYPE_OPEN, 1, 0, 5));
     EXPECT_FALSE(eya_interval_contains_value(EYA_INTERVAL_TYPE_OPEN, 1, 6, 5));
-}
-
-TEST(eya_interval_contains_range, closed_intervals)
-{
-    EXPECT_TRUE(eya_interval_contains_range(EYA_INTERVAL_TYPE_CLOSED, 1, 10, 2, 8));
-    EXPECT_TRUE(eya_interval_contains_range(EYA_INTERVAL_TYPE_CLOSED, 1, 10, 1, 10));
-    EXPECT_FALSE(eya_interval_contains_range(EYA_INTERVAL_TYPE_CLOSED, 1, 5, 0, 6));
-    EXPECT_FALSE(eya_interval_contains_range(EYA_INTERVAL_TYPE_CLOSED, 1, 5, 2, 6));
-}
-
-TEST(eya_interval_contains_range, left_open_intervals)
-{
-    EXPECT_TRUE(eya_interval_contains_range(EYA_INTERVAL_TYPE_LEFT_OPEN, 1, 10, 2, 8));
-    EXPECT_TRUE(eya_interval_contains_range(EYA_INTERVAL_TYPE_LEFT_OPEN, 1, 10, 2, 10));
-    EXPECT_FALSE(eya_interval_contains_range(EYA_INTERVAL_TYPE_LEFT_OPEN, 1, 5, 1, 5));
-    EXPECT_FALSE(eya_interval_contains_range(EYA_INTERVAL_TYPE_LEFT_OPEN, 1, 5, 0, 6));
-}
-
-TEST(eya_interval_contains_range, right_open_intervals)
-{
-    EXPECT_TRUE(eya_interval_contains_range(EYA_INTERVAL_TYPE_RIGHT_OPEN, 1, 10, 2, 8));
-    EXPECT_TRUE(eya_interval_contains_range(EYA_INTERVAL_TYPE_RIGHT_OPEN, 1, 10, 1, 8));
-    EXPECT_FALSE(eya_interval_contains_range(EYA_INTERVAL_TYPE_RIGHT_OPEN, 1, 5, 2, 5));
-    EXPECT_FALSE(eya_interval_contains_range(EYA_INTERVAL_TYPE_RIGHT_OPEN, 1, 5, 0, 6));
-}
-
-TEST(eya_interval_contains_range, open_intervals)
-{
-    EXPECT_TRUE(eya_interval_contains_range(EYA_INTERVAL_TYPE_OPEN, 1, 10, 2, 8));
-    EXPECT_FALSE(eya_interval_contains_range(EYA_INTERVAL_TYPE_OPEN, 1, 5, 1, 5));
-    EXPECT_FALSE(eya_interval_contains_range(EYA_INTERVAL_TYPE_OPEN, 1, 5, 2, 5));
-    EXPECT_FALSE(eya_interval_contains_range(EYA_INTERVAL_TYPE_OPEN, 1, 5, 0, 6));
 }
