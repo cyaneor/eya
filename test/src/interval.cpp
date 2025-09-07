b460d63cc62f137bd3ee3441885a422bf366ed72
Egor Tsyganchuk <cyaneor@gmail.com>
2025-09-07 20:27:22 +0300

    Changed interval_size check tests

--- test/src/interval.cpp
+++ test/src/interval.cpp
@@ -10,7 +10,7 @@ TEST(eya_interval_contains_value_closed, integer_values)
     EXPECT_FALSE(eya_interval_contains_value(EYA_INTERVAL_TYPE_CLOSED, 1, 6, 5));
 }
 
-TEST(eya_interval_contains_value_closed_float, float_values)
+TEST(eya_interval_contains_value_closed, float_values)
 {
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_CLOSED, 1.0, 3.0, 5.0));
     EXPECT_TRUE(eya_interval_contains_value(EYA_INTERVAL_TYPE_CLOSED, 1.0, 1.0, 5.0));
@@ -48,30 +48,22 @@ TEST(eya_interval_contains_value_open, integer_values)
 
 TEST(eya_interval_size, closed_interval)
 {
-    int min = 1;
-    int max = 5;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_CLOSED, min, max), 5);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_CLOSED, 1, 5), 5);
 }
 
 TEST(eya_interval_size, left_open_interval)
 {
-    int min = 1;
-    int max = 5;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_LEFT_OPEN, min, max), 4);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_LEFT_OPEN, 1, 5), 4);
 }
 
 TEST(eya_interval_size, right_open_interval)
 {
-    int min = 1;
-    int max = 5;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_RIGHT_OPEN, min, max), 4);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_RIGHT_OPEN, 1, 5), 4);
 }
 
 TEST(eya_interval_size, open_interval)
 {
-    int min = 1;
-    int max = 5;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_OPEN, min, max), 3);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_OPEN, 1, 5), 3);
 }
 
 TEST(eya_interval_size, invalid_interval_type)
@@ -81,70 +73,50 @@ TEST(eya_interval_size, invalid_interval_type)
 
 TEST(eya_interval_size, closed_interval_min_equals_max)
 {
-    int min = 5;
-    int max = 5;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_CLOSED, min, max), 1);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_CLOSED, 5, 5), 1);
 }
 
 TEST(eya_interval_size, left_open_interval_min_equals_max)
 {
-    int min = 5;
-    int max = 5;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_LEFT_OPEN, min, max), 0);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_LEFT_OPEN, 5, 5), 0);
 }
 
 TEST(eya_interval_size, right_open_interval_min_equals_max)
 {
-    int min = 5;
-    int max = 5;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_RIGHT_OPEN, min, max), 0);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_RIGHT_OPEN, 5, 5), 0);
 }
 
 TEST(eya_interval_size, open_interval_min_equals_max)
 {
-    int min = 5;
-    int max = 5;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_OPEN, min, max), -1);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_OPEN, 5, 5), -1);
 }
 
 TEST(eya_interval_size, closed_interval_negative_range)
 {
-    int min = 5;
-    int max = 1;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_CLOSED, min, max), -3);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_CLOSED, 5, 1), -3);
 }
 
 TEST(eya_interval_size, left_open_interval_negative_range)
 {
-    int min = 5;
-    int max = 1;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_LEFT_OPEN, min, max), -4);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_LEFT_OPEN, 5, 1), -4);
 }
 
 TEST(eya_interval_size, right_open_interval_negative_range)
 {
-    int min = 5;
-    int max = 1;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_RIGHT_OPEN, min, max), -4);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_RIGHT_OPEN, 5, 1), -4);
 }
 
 TEST(eya_interval_size, open_interval_negative_range)
 {
-    int min = 5;
-    int max = 1;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_OPEN, min, max), -5);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_OPEN, 5, 1), -5);
 }
 
 TEST(eya_interval_size, zero_range_closed)
 {
-    int min = 0;
-    int max = 0;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_CLOSED, min, max), 1);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_CLOSED, 0, 0), 1);
 }
 
 TEST(eya_interval_size, large_values_closed)
 {
-    int min = 1000000;
-    int max = 1000005;
-    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_CLOSED, min, max), 6);
+    EXPECT_EQ(eya_interval_size(EYA_INTERVAL_TYPE_CLOSED, 1000000, 1000005), 6);
 }
