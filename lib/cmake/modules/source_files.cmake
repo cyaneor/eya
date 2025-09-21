f26e867ac97659f9c58760a5c43ae3cf112ae9f8
Egor Tsyganchuk <cyaneor@gmail.com>
2025-09-21 19:17:28 +0300

    Statical tests of types are removed

--- lib/cmake/modules/source_files.cmake
+++ lib/cmake/modules/source_files.cmake
@@ -15,12 +15,7 @@ list(APPEND EYA_TARGET_SOURCE_FILES
         ${EYA_LIB_SOURCE_DIR}/eya/runtime_exception_catch_stack.c
         ${EYA_LIB_SOURCE_DIR}/eya/runtime_allocator.c
         ${EYA_LIB_SOURCE_DIR}/eya/runtime_terminate.c
-
-        # Asserts
-        ${EYA_LIB_SOURCE_DIR}/eya/addr.c
-        ${EYA_LIB_SOURCE_DIR}/eya/numeric_fixed_types.c
-        ${EYA_LIB_SOURCE_DIR}/eya/numeric_fixed_limits.c
-
+        
         # Error
         ${EYA_LIB_SOURCE_DIR}/eya/error.c
 
--- lib/src/eya/addr.c
+++
@@ -1,8 +0,0 @@
-#include <eya/addr.h>
-#include <eya/static_assert.h>
-
-eya_static_assert(sizeof(eya_uaddr_t) == sizeof(void *),
-                  "eya_uaddr_t size must exactly match pointer size");
-
-eya_static_assert(sizeof(eya_saddr_t) == sizeof(void *),
-                  "eya_saddr_t size must exactly match pointer size");
